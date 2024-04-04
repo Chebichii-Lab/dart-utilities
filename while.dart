3696051320f2108bfaa7a15dbcb5dc8979c50321
Chebichii-Lab <chebichiiserem@gmail.com>
2024-04-04 06:42:55 +0300

    create a while loop

---
+++ while.dart
@@ -0,0 +1,10 @@
+// Create a program that uses a while loop to print out the numbers from 20 to 10.
+
+void main() {
+  int i = 20; // Starting number
+  
+  while (i >= 10) {
+    print(i);
+    i--; // Decrementing i in each iteration
+  }
+}
