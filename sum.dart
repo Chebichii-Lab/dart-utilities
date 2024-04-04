b856ff289e04027a63b88bac308303dfb5b9b880
Chebichii-Lab <chebichiiserem@gmail.com>
2024-04-04 06:27:30 +0300

    add a sum function

---
+++ sum.dart
@@ -0,0 +1,11 @@
+// Create a function that takes two numbers as input and returns the sum of those numbers.
+
+void main() {
+  int num1 = 15;
+  int num2 = 20;
+
+  num sum = num1 + num2;
+
+  print('The sum is $num1 + $num2');
+  print('Sum is $sum');
+}
