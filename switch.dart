6b774450c0311b353d427a3f507f5e7210814f7d
Chebichii-Lab <chebichiiserem@gmail.com>
2024-04-04 06:41:21 +0300

    create a switch statement

---
+++ switch.dart
@@ -0,0 +1,20 @@
+// Program to use a switch statement
+
+void main() {
+  String input = 'apple'; // Change this value to test different cases
+  
+  switch (input) {
+    case 'apple':
+      print('You chose an apple.');
+      break;
+    case 'banana':
+      print('You chose a banana.');
+      break;
+    case 'orange':
+      print('You chose an orange.');
+      break;
+    default:
+      print('Unknown fruit.');
+  }
+}
+
